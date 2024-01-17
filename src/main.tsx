a8cf60fe40762dde64a49d92fa33e74908e63c56
Johannes Brodwall <johannes@brodwall.com>
2024-01-17 17:01:08 +0100

    Add openlayers

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, {MutableRefObject, useEffect, useRef} from "react";
 import ReactDOM from "react-dom/client";
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import {useGeographic} from "ol/proj";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<h1>Hello React World</h1>);
+
+
+useGeographic();
+const map = new Map({
+    layers: [
+        new TileLayer({source: new OSM()})
+    ],
+    view: new View({
+        center: [11, 59], zoom: 10
+    })
+})
+
+function MapApplication() {
+    const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+    useEffect(() => {
+        map.setTarget(mapRef.current);
+    }, []);
+    return <div ref={mapRef}></div>;
+}
+
+root.render(<MapApplication />);
 
